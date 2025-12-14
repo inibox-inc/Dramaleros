27dab8e73be67d1676f88e439a4aef09478bfd74
inibox-inc <inibox.app@gmail.com>
2025-12-14 13:13:02 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// src/main.jsx (Si usas Vite)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+// Importa estilos globales si los tienes (opcional)
+// import './index.css' 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
